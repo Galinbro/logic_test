39cf9ca3ed81b4449185071aac931d7617e1412c
Galinbro <galindo9810@hotmail.com>
2020-02-16 23:58:13 -0600

    finish task

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,30 @@
+fun main(args: Array<String>){
+    test(3,3)
 
-    fun main(){
-        println("Hellow")
+}
+
+
+fun test(rows: Int, columns: Int){
+    if (columns == 1) {
+        if (rows == 1)
+            println("R")
+        else
+            println("D")
+    }else if (rows > columns && columns > 1) {
+        if (columns % 2 == 0)
+            println("U")
+        else
+            println("D")
+    }else if (columns > rows) {
+        if (rows % 2 == 0)
+            println("L")
+        else
+            println("R")
+    }else if (rows == columns && rows > 1) {
+        if (rows % 2 == 0)
+            println("L")
+        else
+            println("R")
     }
 
+}
